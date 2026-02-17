f6d87ed7b927badd156ad4570a1d43ad46d2c674
Francesco Saverio <garzicosentino@gmail.com>
2026-02-17 22:21:03 +0100

    feat: implement background Worker

---
+++ include/slog/async/async_op.hpp
@@ -0,0 +1,16 @@
+#ifndef SLOG_ASYNC_ASYNC_OP_HPP
+#define SLOG_ASYNC_ASYNC_OP_HPP
+
+#include <slog/core/log_record.hpp>
+#include <slog/sinks/sink_manager.hpp>
+
+namespace slog::async
+{
+    struct AsyncOp
+    {
+        slog::LogRecord record;
+        slog::sinks::SinkManager* sink_manager;
+    };
+}
+
+#endif // SLOG_ASYNC_ASYNC_OP_HPP
---
+++ include/slog/async/worker.hpp
@@ -0,0 +1,93 @@
+#ifndef SLOG_ASYNC_WORKER_HPP
+#define SLOG_ASYNC_WORKER_HPP
+
+#ifdef SLOG_ASYNC_ENABLED
+
+#include <atomic>
+#include <new>
+#include <thread>
+
+#include <slog/core/common.hpp>
+#include <slog/async/async_op.hpp>
+#include <slog/async/mpsc_queue.hpp>
+#include <slog/sinks/sink_manager.hpp>
+
+namespace slog::async
+{
+
+template<typename Policy, size_t Size = 8192>
+class Worker
+{
+    public:
+        Worker() : _running(true), _worker_thread(&Worker::_loop, this) {}
+
+        Worker(const Worker&) = delete;
+        Worker(Worker&&) = delete;
+
+        Worker& operator=(const Worker&) = delete;
+        Worker& operator=(Worker&&) = delete;
+
+        ~Worker()
+        {
+            stop();
+            join();
+        }
+
+        SLOG_ALWAYS_INLINE void stop()
+        {
+            _running.store(false, std::memory_order_release);
+        }
+
+        SLOG_ALWAYS_INLINE void join()
+        {
+            if (_worker_thread.joinable()) {
+                _worker_thread.join();
+            }
+        }
+
+        SLOG_ALWAYS_INLINE bool push(AsyncOp&& op)
+        {
+            bool ret = _queue.push(std::move(op));
+
+            if (ret && !_flag.exchange(true, std::memory_order_release)) {
+                _flag.notify_one();
+            }
+            return ret;
+        }
+
+    private:
+
+        void _loop()
+        {
+            AsyncOp op;
+
+            while (_running.load(std::memory_order_relaxed)) {
+                while (_queue.pop(op)) {
+                    op.sink_manager->dispatch(op.record);
+                }
+
+                _flag.store(false, std::memory_order_release);
+                if (!_queue.pop(op)) {
+                    _flag.wait(false, std::memory_order_acquire);
+                }
+                else {
+                    op.sink_manager->dispatch(op.record);
+                }
+            }
+
+            while (_queue.pop(op)) {
+                op.sink_manager->dispatch(op.record);
+            }
+        }
+
+        MPSCQueue<AsyncOp, Size, Policy>     _queue;
+        std::thread                          _worker_thread;
+        alignas(std::hardware_destructive_interference_size) std::atomic<bool>  _running;
+        alignas(std::hardware_destructive_interference_size) std::atomic<bool>  _flag{true};
+};
+
+}
+
+#endif // SLOG_ASYNC_ENABLED
+
+#endif // SLOG_ASYNC_WORKER_HPP
